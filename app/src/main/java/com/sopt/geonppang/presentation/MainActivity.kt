959be8ee6b6dd765c53ce1c21c61c928436b9e95
Dani43 <eldms0406@naver.com>
2023-07-07 14:32:18 +0900

    [feat] #3 임의로 MainActivity에서 HomeFragment 뜨도록 구현

--- app/src/main/java/com/sopt/geonppang/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.sopt.geonppang.presentation
 
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.ActivityMainBinding
+import com.sopt.geonppang.presentation.home.HomeFragment
 import com.sopt.geonppang.util.binding.BindingActivity
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO 삭제
+        supportFragmentManager.commit {
+            replace(R.id.fcv_container, HomeFragment())
+        }
     }
 }
