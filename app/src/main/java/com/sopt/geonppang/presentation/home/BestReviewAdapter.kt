e041dd8e6dff42c8ede1ed8307a1a3a317345525
Dani43 <eldms0406@naver.com>
2023-07-07 14:30:47 +0900

    [feat] #3 베스트 리뷰 adapter 구현

---
+++ app/src/main/java/com/sopt/geonppang/presentation/home/BestReviewAdapter.kt
@@ -0,0 +1,36 @@
+package com.sopt.geonppang.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.geonppang.databinding.ItemHomeBestReviewBinding
+import com.sopt.geonppang.domain.model.BestReview
+import com.sopt.geonppang.util.ItemDiffCallback
+
+class BestReviewAdapter : ListAdapter<BestReview, BestReviewAdapter.ReviewViewHolder>(
+    ItemDiffCallback<BestReview>(
+        onItemsTheSame = { old, new -> old.bakeryId == new.bakeryId },
+        onContentsTheSame = { old, new -> old == new }
+    )
+) {
+
+    class ReviewViewHolder(
+        private val binding: ItemHomeBestReviewBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(review: BestReview) {
+            binding.review = review
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        val binding =
+            ItemHomeBestReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ReviewViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+}
