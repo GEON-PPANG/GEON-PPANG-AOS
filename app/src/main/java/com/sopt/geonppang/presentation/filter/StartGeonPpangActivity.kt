35e425a8cc10daf72b35f23d726576c47e77197c
jihyunniiii <jita1125@naver.com>
2023-07-10 02:36:11 +0900

    [feat] #4 StartGeonPpangActivity 구현

---
+++ app/src/main/java/com/sopt/geonppang/presentation/filter/StartGeonPpangActivity.kt
@@ -0,0 +1,15 @@
+package com.sopt.geonppang.presentation.filter
+
+import android.os.Bundle
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.ActivityStartGeonPpangBinding
+import com.sopt.geonppang.util.binding.BindingActivity
+
+class StartGeonPpangActivity :
+    BindingActivity<ActivityStartGeonPpangBinding>(R.layout.activity_start_geon_ppang) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
