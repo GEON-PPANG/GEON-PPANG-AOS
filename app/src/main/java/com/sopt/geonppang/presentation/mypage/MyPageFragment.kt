72746f6e4cf3d7e9d8549985214d85222e0a6df8
jihyunniiii <jita1125@naver.com>
2023-07-11 10:12:44 +0900

    [feat] #11 마이페이지 구현

---
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/MyPageFragment.kt
@@ -0,0 +1,19 @@
+package com.sopt.geonppang.presentation.mypage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.FragmentMyPageBinding
+import com.sopt.geonppang.util.binding.BindingFragment
+
+class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private val viewModel by viewModels<MyPageViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+    }
+}
