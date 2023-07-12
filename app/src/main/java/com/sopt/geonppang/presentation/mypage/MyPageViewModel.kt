2e099984fd39315e71c18a2bd796ed49c5d4c47a
jihyunniiii <jita1125@naver.com>
2023-07-12 14:45:51 +0900

    [chore] #11 dataclass 수정으로 인한 ViewModel 수정

--- app/src/main/java/com/sopt/geonppang/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/MyPageViewModel.kt
@@ -1,13 +1,14 @@
 package com.sopt.geonppang.presentation.mypage
 
 import androidx.lifecycle.ViewModel
-import com.sopt.geonppang.presentation.dummy.Profile
+import com.sopt.geonppang.domain.model.BreadType
+import com.sopt.geonppang.domain.model.Profile
 
 class MyPageViewModel : ViewModel() {
     private val _profile = Profile(
         memberNickname = "안빵이들",
         mainPurpose = "맛 . 다이어트",
-        breadType = Profile.BreadType(
+        breadType = BreadType(
             isGlutenFree = true,
             isVegan = true,
             isNutFree = false,
