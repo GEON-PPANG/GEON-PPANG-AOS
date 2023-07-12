b2aba227972df1f2b10e43a749fc47b54dc4b14a
jihyunniiii <jita1125@naver.com>
2023-07-12 14:05:06 +0900

    [feat] #18 상세페이지 Activity 생성

---
+++ app/src/main/java/com/sopt/geonppang/presentation/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.geonppang.presentation.detail
+
+import android.os.Bundle
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.ActivityDetailBinding
+import com.sopt.geonppang.util.binding.BindingActivity
+
+class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
