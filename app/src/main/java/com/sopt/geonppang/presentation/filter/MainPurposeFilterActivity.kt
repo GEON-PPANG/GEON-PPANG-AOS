96698a9abc8b66057bd150987ef6ef9907137834
Dani43 <eldms0406@naver.com>
2023-07-12 12:07:38 +0900

    [mod] #4 mainPurpose 뷰 수정

--- app/src/main/java/com/sopt/geonppang/presentation/filter/MainPurposeFilterActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/filter/MainPurposeFilterActivity.kt
@@ -10,22 +10,13 @@ import com.sopt.geonppang.util.binding.BindingActivity
 class MainPurposeFilterActivity :
     BindingActivity<ActivityMainPurposeFilterBinding>(R.layout.activity_main_purpose_filter) {
     private val viewModel by viewModels<FilterViewModel>()
-    private lateinit var adapter: MainPurposeFilterAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
-        initLayout()
         addListeners()
-    }
-
-    private fun initLayout() {
-        adapter = MainPurposeFilterAdapter(this, viewModel)
-        adapter.submitList(viewModel.mainPurposeFilterList)
-
-        binding.rvMainPurposeFilterOption.adapter = adapter
     }
 
     private fun addListeners() {
