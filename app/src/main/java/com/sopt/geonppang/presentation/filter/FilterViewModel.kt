afb37ac2f5bada506436515f957e02064b94fcfb
Dani43 <eldms0406@naver.com>
2023-07-13 18:36:57 +0900

    [feat] #4 user의 BreadFilterType을 setting하는 함수 구현

--- app/src/main/java/com/sopt/geonppang/presentation/filter/FilterViewModel.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/filter/FilterViewModel.kt
@@ -3,32 +3,12 @@ package com.sopt.geonppang.presentation.filter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.sopt.geonppang.domain.model.Filter
 import com.sopt.geonppang.domain.model.NutrientFilter
 import com.sopt.geonppang.presentation.type.MainPurposeType
+import com.sopt.geonppang.presentation.type.BreadFilterType
 import timber.log.Timber
 
 class FilterViewModel : ViewModel() {
-    private val _breadTypeFilterList = listOf(
-        Filter(
-            title = "글루텐프리",
-            detail = "NO 글루텐 포함\n밀 , 곡물류"
-        ),
-        Filter(
-            title = "비건빵",
-            detail = "NO 동물성재료\n(유제품 , 계란)"
-        ),
-        Filter(
-            title = "넛프리",
-            detail = "NO 견과류"
-        ),
-        Filter(
-            title = "저당 , 무설탕",
-            detail = "대체당 사용"
-        )
-    )
-    val breadTypeFilterList = _breadTypeFilterList
-
     private val _nutrientTypeFilterList = listOf(
         NutrientFilter(
             title = "영양성분 공개"
@@ -49,13 +29,29 @@ class FilterViewModel : ViewModel() {
         _mainPurpose.value = mainPurposeType
     }
 
+    val breadFilterType: MutableLiveData<Map<BreadFilterType, Boolean>> = MutableLiveData(
+        mapOf(
+            BreadFilterType.GLUTENFREE to false,
+            BreadFilterType.VEGAN to false,
+            BreadFilterType.NUTFREE to false,
+            BreadFilterType.SUGARFREE to false
+        )
+    )
+
+    fun setUserBreadType(breadType: BreadFilterType) {
+        val isSelected = breadFilterType.value?.get(breadType) ?: return
+        breadFilterType.value = breadFilterType.value?.toMutableMap()?.apply {
+            this[breadType] = !isSelected
+        }
+    }
+
     val nutrientTypeFilterList = _nutrientTypeFilterList
 
-    private val _mainPurposeFilterSelectedItemIndex: MutableLiveData<Int> = MutableLiveData()
-    val mainPurposeFilterSelectedItemIndex: LiveData<Int> = _mainPurposeFilterSelectedItemIndex
-
-    private val _breadTypeFilterSelectedItemList: MutableLiveData<List<Int>> = MutableLiveData()
-    val breadTypeFilterSelectedItemList: LiveData<List<Int>> = _breadTypeFilterSelectedItemList
+//    private val _mainPurposeFilterSelectedItemIndex: MutableLiveData<Int> = MutableLiveData()
+//    val mainPurposeFilterSelectedItemIndex: LiveData<Int> = _mainPurposeFilterSelectedItemIndex
+//
+//    private val _breadTypeFilterSelectedItemList: MutableLiveData<List<Int>> = MutableLiveData()
+//    val breadTypeFilterSelectedItemList: LiveData<List<Int>> = _breadTypeFilterSelectedItemList
 
     private val _nutrientTypeFilterSelectedItemList: MutableLiveData<List<Int>> = MutableLiveData()
     val nutrientTypeFilterSelectedItemList: LiveData<List<Int>> =
@@ -64,27 +60,27 @@ class FilterViewModel : ViewModel() {
     private val _isNextBtnEnabled: MutableLiveData<Boolean> = MutableLiveData()
     val isNextBtnEnabled: LiveData<Boolean> = _isNextBtnEnabled
 
-    fun setMainPurposeFilterSelectedItemIndex(index: Int) {
-        _mainPurposeFilterSelectedItemIndex.value = index
-        _isNextBtnEnabled.value = (index != -1)
-    }
+//    fun setMainPurposeFilterSelectedItemIndex(index: Int) {
+//        _mainPurposeFilterSelectedItemIndex.value = index
+//        _isNextBtnEnabled.value = (index != -1)
+//    }
 
     fun setIsNextBtnEnabled(value: Boolean) {
         _isNextBtnEnabled.value = value
     }
 
-    fun setBreadTypeFilterSelectedItemList(index: Int) {
-        val currentList = _breadTypeFilterSelectedItemList.value.orEmpty().toMutableList()
-
-        if (currentList.contains(index)) currentList.remove(index)
-        else {
-            currentList.add(index)
-            currentList.sort()
-        }
-
-        _breadTypeFilterSelectedItemList.value = currentList
-        _isNextBtnEnabled.value = (!currentList.isEmpty())
-    }
+//    fun setBreadTypeFilterSelectedItemList(index: Int) {
+//        val currentList = _breadTypeFilterSelectedItemList.value.orEmpty().toMutableList()
+//
+//        if (currentList.contains(index)) currentList.remove(index)
+//        else {
+//            currentList.add(index)
+//            currentList.sort()
+//        }
+//
+//        _breadTypeFilterSelectedItemList.value = currentList
+//        _isNextBtnEnabled.value = (!currentList.isEmpty())
+//    }
 
     fun setNutrientTypeFilterSelectedItemList(index: Int) {
         val currentList = _nutrientTypeFilterSelectedItemList.value.orEmpty().toMutableList()
