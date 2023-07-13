397f69fe73509cc80f22bdcbc784633a3cb61050
Dani43 <eldms0406@naver.com>
2023-07-13 21:11:58 +0900

    [feat] #21 저장 목록 리스트 엑티비티 및 레이아웃 파일 생성

---
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/MyStoreListActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.geonppang.presentation.mypage
+
+import android.os.Bundle
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.ActivityMyStoreListBinding
+import com.sopt.geonppang.util.binding.BindingActivity
+
+class MyStoreListActivity :
+    BindingActivity<ActivityMyStoreListBinding>(R.layout.activity_my_store_list) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
