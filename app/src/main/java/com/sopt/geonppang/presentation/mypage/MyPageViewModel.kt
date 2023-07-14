85f00bd65cce810bdffaeb5b3f87bb722a84b869
Dani43 <eldms0406@naver.com>
2023-07-14 13:33:15 +0900

    [feat] #26 내 리뷰 모아보기 뷰 구현 및 더미 데이터 추가

--- app/src/main/java/com/sopt/geonppang/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/MyPageViewModel.kt
@@ -1,8 +1,11 @@
 package com.sopt.geonppang.presentation.mypage
 
 import androidx.lifecycle.ViewModel
+import com.sopt.geonppang.R
+import com.sopt.geonppang.domain.model.Bakery
 import com.sopt.geonppang.domain.model.BreadType
 import com.sopt.geonppang.domain.model.Profile
+import com.sopt.geonppang.domain.model.Review
 
 class MyPageViewModel : ViewModel() {
     private val _profile = Profile(
@@ -18,4 +21,79 @@ class MyPageViewModel : ViewModel() {
         )
     )
     val profile = _profile
+
+    val mockMyReviewList = listOf(
+        Review(
+            reviewId = 1,
+            date = "23.07.14",
+            bakery = Bakery(
+                bakeryId = 1,
+                bakeryName = "건대 비건빵아아아아아아아아아",
+                firstNearStation = "건대역",
+                secondNearStation = "성수역",
+                isBooked = true,
+                bookmarkCount = 5,
+                bakeryPicture = R.drawable.bbang1,
+                isHACCP = true,
+                isNonGMO = true,
+                isVegan = true,
+                breadType = BreadType(
+                    breadTypeId = 1,
+                    breadTypeName = "글루텐프리",
+                    isGlutenFree = true,
+                    isVegan = true,
+                    isNutFree = true,
+                    isSugarFree = true,
+                )
+            )
+        ),
+        Review(
+            reviewId = 2,
+            date = "23.07.15",
+            bakery = Bakery(
+                bakeryId = 1,
+                bakeryName = "건대 비건빵아아아아아아아아아",
+                firstNearStation = "동대문역사공원역",
+                secondNearStation = null,
+                isBooked = false,
+                bookmarkCount = 6,
+                bakeryPicture = R.drawable.bbang1,
+                isHACCP = false,
+                isNonGMO = true,
+                isVegan = true,
+                breadType = BreadType(
+                    breadTypeId = 1,
+                    breadTypeName = "글루텐프리",
+                    isGlutenFree = true,
+                    isVegan = false,
+                    isNutFree = false,
+                    isSugarFree = true,
+                )
+            )
+        ),
+        Review(
+            reviewId = 3,
+            date = "23.07.16",
+            bakery = Bakery(
+                bakeryId = 1,
+                bakeryName = "건대 비건빵아아아아아아아아아",
+                firstNearStation = "건대역",
+                secondNearStation = null,
+                isBooked = false,
+                bookmarkCount = 0,
+                bakeryPicture = R.drawable.bbang1,
+                isHACCP = false,
+                isNonGMO = true,
+                isVegan = true,
+                breadType = BreadType(
+                    breadTypeId = 1,
+                    breadTypeName = "글루텐프리",
+                    isGlutenFree = true,
+                    isVegan = false,
+                    isNutFree = true,
+                    isSugarFree = true,
+                )
+            )
+        ),
+    )
 }
---
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/MyReviewActivity.kt
@@ -0,0 +1,26 @@
+package com.sopt.geonppang.presentation.mypage
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.ActivityMyReviewBinding
+import com.sopt.geonppang.util.binding.BindingActivity
+
+class MyReviewActivity : BindingActivity<ActivityMyReviewBinding>(R.layout.activity_my_review) {
+    private val viewModel: MyPageViewModel by viewModels()
+
+    lateinit var myReviewAdapter: MyReviewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+        myReviewAdapter = MyReviewAdapter()
+        binding.rvMyReviewList.adapter = myReviewAdapter
+        myReviewAdapter.submitList(viewModel.mockMyReviewList)
+    }
+}
