ace5e515b2f6abab7fb816b50be7cc9eedebddf6
jihyunniiii <jita1125@naver.com>
2023-07-14 09:07:31 +0900

    [feat] #18 DetailNoReviewAdapter 구현

---
+++ app/src/main/java/com/sopt/geonppang/presentation/detail/DetailNoReviewAdapter.kt
@@ -0,0 +1,42 @@
+package com.sopt.geonppang.presentation.detail
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.geonppang.databinding.ItemDetailNoReviewBinding
+import com.sopt.geonppang.domain.model.ReviewData
+import com.sopt.geonppang.util.ItemDiffCallback
+
+class DetailNoReviewAdapter(context: Context) :
+    ListAdapter<ReviewData, DetailNoReviewAdapter.DetailNoReviewDataViewHolder>(
+        ItemDiffCallback<ReviewData>(
+            onContentsTheSame = { old, new -> old == new },
+            onItemsTheSame = { old, new -> old == new }
+        )
+    ) {
+    private val inflater by lazy { LayoutInflater.from(context) }
+
+    class DetailNoReviewDataViewHolder(private val binding: ItemDetailNoReviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {}
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DetailNoReviewDataViewHolder {
+        val binding = ItemDetailNoReviewBinding.inflate(inflater, parent, false)
+        return DetailNoReviewDataViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: DetailNoReviewDataViewHolder, position: Int
+    ) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+}
