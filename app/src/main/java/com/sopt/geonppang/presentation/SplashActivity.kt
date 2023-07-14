d1742667748836b7eba8a89d81e5c7429d9d2651
jooyyoo <aliceyun9@naver.com>
2023-07-14 23:51:18 +0900

    [feat] #28 스플래시 뷰 구현

---
+++ app/src/main/java/com/sopt/geonppang/presentation/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.sopt.geonppang.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.ActivitySplashBinding
+import com.sopt.geonppang.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        loadSplashScreen()
+    }
+
+    private fun loadSplashScreen() {
+        lifecycleScope.launch {
+            delay(1500L)
+            moveToNext()
+            finish()
+        }
+    }
+
+    private fun moveToNext() {
+        Log.e("this", "이것은 되는가?")
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
