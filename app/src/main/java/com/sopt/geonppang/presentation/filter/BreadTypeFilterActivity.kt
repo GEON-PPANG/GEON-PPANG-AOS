df5478fed54176196e6edb9abf3b909632761c8b
jihyunniiii <jita1125@naver.com>
2023-07-15 23:59:13 +0900

    [chore] #4 BreadTypeFilterActivity intent 수정

--- app/src/main/java/com/sopt/geonppang/presentation/filter/BreadTypeFilterActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/filter/BreadTypeFilterActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.ActivityBreadTypeFilterBinding
-import com.sopt.geonppang.domain.model.BreadType
-import com.sopt.geonppang.presentation.type.BreadFilterType
 import com.sopt.geonppang.util.binding.BindingActivity
-import timber.log.Timber
 
 class BreadTypeFilterActivity :
     BindingActivity<ActivityBreadTypeFilterBinding>(R.layout.activity_bread_type_filter) {
@@ -28,21 +25,8 @@ class BreadTypeFilterActivity :
         }
 
         binding.btnBreadTypeFilterNext.setOnClickListener {
-            val userType = with(viewModel.breadFilterType.value) {
-                BreadType(
-                    this?.get(BreadFilterType.GLUTENFREE) == true,
-                    this?.get(BreadFilterType.VEGAN) == true,
-                    this?.get(BreadFilterType.NUTFREE) == true,
-                    this?.get(BreadFilterType.SUGARFREE) == true
-                )
-            }
-            moveToNutrientTypeFilter()
-            Timber.d(userType.toString())
+            val intent = Intent(this, NutrientTypeFilterActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    private fun moveToNutrientTypeFilter() {
-        val intent = Intent(this, NutrientTypeFilterActivity::class.java)
-        startActivity(intent)
     }
 }
