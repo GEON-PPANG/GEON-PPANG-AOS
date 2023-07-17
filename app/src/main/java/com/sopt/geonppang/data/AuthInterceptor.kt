53739b3a67d4d56f561219acdd7bf12921c87cf4
Dani43 <eldms0406@naver.com>
2023-07-17 16:03:58 +0900

    [init] #32 AuthInterceptor 구현

---
+++ app/src/main/java/com/sopt/geonppang/data/AuthInterceptor.kt
@@ -0,0 +1,23 @@
+package com.sopt.geonppang.data
+
+import com.sopt.geonppang.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val authRequest =
+            originalRequest.newBuilder().addHeader("accesstoken", BuildConfig.ACCESS_TOKEN).build()
+        val response = chain.proceed(authRequest)
+
+        when (response.code) {
+            401 -> {
+                // TODO 토큰 재발급 api 연동
+            }
+        }
+        return response
+    }
+}
--- app/src/main/java/com/sopt/geonppang/di/NetworkModule.kt
+++ app/src/main/java/com/sopt/geonppang/di/NetworkModule.kt
@@ -3,12 +3,14 @@ package com.sopt.geonppang.di
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sopt.geonppang.BuildConfig
 import com.sopt.geonppang.BuildConfig.DEBUG
+import com.sopt.geonppang.data.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -40,11 +42,18 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideOkHttpClientBuilder(): OkHttpClient =
+    fun provideAuthInterceptor(interceptor: AuthInterceptor): Interceptor = interceptor
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClientBuilder(
+        interceptor: AuthInterceptor,
+    ): OkHttpClient =
         OkHttpClient.Builder().apply {
             connectTimeout(10, TimeUnit.SECONDS)
             writeTimeout(10, TimeUnit.SECONDS)
             readTimeout(10, TimeUnit.SECONDS)
+            addInterceptor(interceptor)
             if (DEBUG) addInterceptor(
                 HttpLoggingInterceptor().apply {
                     level = HttpLoggingInterceptor.Level.BODY
