80d56266de0ab79fc9772406a47119cd48950ace
jooyyoo <aliceyun9@naver.com>
2023-07-20 01:52:43 +0900

    [del] #44 마이페이지 북마크한 빵집 모아보기 파일 삭제

--- app/src/main/java/com/sopt/geonppang/data/service/MypageBookmarkService.kt
+++
@@ -1,9 +0,0 @@
-package com.sopt.geonppang.data.service
-
-import com.sopt.geonppang.data.model.response.ResponseMypageBookmark
-import retrofit2.http.GET
-
-interface MypageBookmarkService {
-    @GET("member/bookMarks")
-    suspend fun fetchMypageBookmark():ResponseMypageBookmark
-}
--- app/src/main/java/com/sopt/geonppang/data/service/MypageReviewService.kt
+++
@@ -1,9 +0,0 @@
-package com.sopt.geonppang.data.service
-
-import com.sopt.geonppang.data.model.response.ResponseMypageReview
-import retrofit2.http.GET
-
-interface MypageReviewService {
-    @GET("member/reviews")
-    suspend fun fethcMypageReveiw(): ResponseMypageReview
-}
