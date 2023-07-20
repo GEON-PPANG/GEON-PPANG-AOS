c49f212545769521ea1bfc5eebab8ae81d492202
Dani43 <eldms0406@naver.com>
2023-07-20 20:55:54 +0900

    [feat] #49 -> 상세페이지로 이동, 상세페이지에서 뒤로 이동하는 로직 구현(임의)

--- app/src/main/java/com/sopt/geonppang/presentation/review/ReviewViewModel.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/review/ReviewViewModel.kt
@@ -1,27 +1,40 @@
 package com.sopt.geonppang.presentation.review
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.sopt.geonppang.data.model.request.RequestWriteReview
-import com.sopt.geonppang.domain.repository.WriteReviewRepository
+import androidx.lifecycle.*
+import com.sopt.geonppang.data.model.request.RequestReviewWriting
+import com.sopt.geonppang.domain.repository.ReviewWritingRepository
+import com.sopt.geonppang.presentation.model.BakeryReviewWritingInfo
 import com.sopt.geonppang.presentation.type.KeyWordType
 import com.sopt.geonppang.presentation.type.LikeType
+import com.sopt.geonppang.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
+import kotlin.collections.set
 
 @HiltViewModel
-class ReviewViewModel @Inject constructor(private val writeReviewRepository: WriteReviewRepository) :
+class ReviewViewModel @Inject constructor(private val reviewWritingRepository: ReviewWritingRepository) :
     ViewModel() {
 
     val reviewText = MutableLiveData("")
-
+    private val _bakeryId: MutableLiveData<Int> = MutableLiveData()
+    val bakeryId: LiveData<Int> = _bakeryId
     private val _isLike: MutableLiveData<LikeType> = MutableLiveData()
     val isLike: LiveData<LikeType> = _isLike
+    private val _reviewSuccessState: MutableLiveData<UiState<Boolean>> = MutableLiveData()
+    val reviewSuccessState: LiveData<UiState<Boolean>> = _reviewSuccessState
+    private val _reviewCancelState: MutableLiveData<Boolean> = MutableLiveData()
+    val reviewCancelState: LiveData<Boolean> = _reviewCancelState
+    private val _bakeryInfo: MutableLiveData<BakeryReviewWritingInfo> = MutableLiveData()
+    val bakeryInfo: LiveData<BakeryReviewWritingInfo> = _bakeryInfo
+
+    fun setBakeryId(bakeryId: Int) {
+        _bakeryId.value = bakeryId
+    }
+
+    fun setReviewCancelState(isCancel: Boolean) {
+        _reviewCancelState.value = isCancel
+    }
 
     fun setLikeType(isLike: LikeType) {
         _isLike.value = isLike
@@ -50,33 +63,40 @@ class ReviewViewModel @Inject constructor(private val writeReviewRepository: Wri
         }
     }
 
-    fun getTrueKeyWordTypes(): List<RequestWriteReview.KeywordName> {
-        val trueKeyWordTypes = mutableListOf<RequestWriteReview.KeywordName>()
+    fun getKeyWordTypeList(): List<RequestReviewWriting.KeywordName> {
+        val trueKeyWordTypes = mutableListOf<RequestReviewWriting.KeywordName>()
         userKeyWordType.value?.forEach { (keyWordType, value) ->
             if (value) {
-                trueKeyWordTypes.add(RequestWriteReview.KeywordName(keyWordType.keywordType))
+                trueKeyWordTypes.add(RequestReviewWriting.KeywordName(keyWordType.keywordType))
             }
         }
         return trueKeyWordTypes
     }
 
-    fun writeReview(bakeryId: Int) {
+
+    fun writeReview() {
         viewModelScope.launch {
             reviewText.value?.let { reviewText ->
-                writeReviewRepository.writeReview(
-                    bakeryId, RequestWriteReview(
-                        _isLike.value == LikeType.LIKE,
-                        getTrueKeyWordTypes(),
-                        reviewText
+                _bakeryId.value?.let {
+                    reviewWritingRepository.writeReview(
+                        it, RequestReviewWriting(
+                            _isLike.value == LikeType.LIKE,
+                            getKeyWordTypeList(),
+                            reviewText
+                        )
                     )
-                )
-                    .onSuccess {
-                        Timber.d(it.message)
-                    }
-                    .onFailure {
-                        Timber.e(it.message)
-                    }
+                        .onSuccess {
+                            _reviewSuccessState.value = UiState.Success(true)
+                        }
+                        .onFailure { throwable ->
+                            _reviewSuccessState.value = UiState.Error(throwable.message)
+                        }
+                }
             }
         }
+    }
+
+    fun setBakeryInfo(bakeryInfo: BakeryReviewWritingInfo) {
+        _bakeryInfo.value = bakeryInfo
     }
 }
