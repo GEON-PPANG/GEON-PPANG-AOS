17009158810ec499ad8e9526969a0739921e3450
jooyyoo <aliceyun9@naver.com>
2023-07-20 13:12:38 +0900

    [del] #35 마이페이지 회원정보 조회 api 파일 통일을 위한 삭제

--- app/src/main/java/com/sopt/geonppang/data/service/MypageInfoService.kt
+++
@@ -1,9 +0,0 @@
-package com.sopt.geonppang.data.service
-
-import com.sopt.geonppang.data.model.response.ResponseMypageInfo
-import retrofit2.http.GET
-
-interface MypageInfoService {
-    @GET("/member")
-    suspend fun fetchMypageInfo() : ResponseMypageInfo
-}
--- app/src/main/java/com/sopt/geonppang/domain/repository/MypageInfoRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.sopt.geonppang.domain.repository
-
-import com.sopt.geonppang.domain.model.Profile
-
-interface MypageInfoRepository {
-    suspend fun fetchMypageInfo(): Result<Profile>
-}
