33f15c631155c5e25ef2e9ec427adc0847904750
Dani43 <eldms0406@naver.com>
2023-08-21 01:52:21 +0900

    [feat] #84 내가 작성한 리뷰 item의 빵집 정보를 리뷰 상세뷰로 전달

--- app/src/main/java/com/sopt/geonppang/presentation/mypage/MyReviewAdapter.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/MyReviewAdapter.kt
@@ -5,9 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.geonppang.databinding.ItemMyReviewBinding
 import com.sopt.geonppang.domain.model.MyReview
+import com.sopt.geonppang.presentation.model.BakeryReviewWritingInfo
+import com.sopt.geonppang.presentation.model.MyReviewBakeryInfo
 import com.sopt.geonppang.util.ItemDiffCallback
 
-class MyReviewAdapter :
+class MyReviewAdapter(
+    private val moveToReviewDetail: (Int, MyReviewBakeryInfo) -> Unit,
+) :
     androidx.recyclerview.widget.ListAdapter<MyReview, MyReviewAdapter.MyReviewViewHolder>(
         ItemDiffCallback<MyReview>(
             onItemsTheSame = { old, new -> old.reviewId == new.reviewId },
@@ -18,8 +22,29 @@ class MyReviewAdapter :
     class MyReviewViewHolder(
         private val binding: ItemMyReviewBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(review: MyReview) {
+        fun onBind(
+            review: MyReview,
+            moveToReviewDetail: (Int, MyReviewBakeryInfo) -> Unit,
+        ) {
             binding.review = review
+            binding.root.setOnClickListener {
+                moveToReviewDetail(
+                    review.reviewId,
+                    MyReviewBakeryInfo(
+                        review.date,
+                        review.bakery.bakeryName,
+                        review.bakery.bakeryPicture,
+                        review.bakery.firstNearStation,
+                        review.bakery.secondNearStation,
+                        BakeryReviewWritingInfo.BreadType(
+                            review.bakery.breadType.isGlutenFree,
+                            review.bakery.breadType.isVegan,
+                            review.bakery.breadType.isNutFree,
+                            review.bakery.breadType.isSugarFree
+                        )
+                    )
+                )
+            }
             binding.executePendingBindings()
         }
     }
@@ -31,6 +56,6 @@ class MyReviewAdapter :
     }
 
     override fun onBindViewHolder(holder: MyReviewViewHolder, position: Int) {
-        holder.onBind(getItem(position))
+        holder.onBind(getItem(position), moveToReviewDetail)
     }
 }
