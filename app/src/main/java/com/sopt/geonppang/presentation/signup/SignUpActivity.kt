83274734955de6bd27b77f866231ebc556f8c9a4
jooyyoo <aliceyun9@naver.com>
2023-08-23 00:10:18 +0900

    [feat] #88 회원가입 뷰에서 뒤로가기 눌렀을 때 로그인화면으로 돌아오기 기능

--- app/src/main/java/com/sopt/geonppang/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/signup/SignUpActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.ActivitySignupBinding
+import com.sopt.geonppang.presentation.login.LoginActivity
 import com.sopt.geonppang.util.binding.BindingActivity
 import com.sopt.geonppang.util.extension.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,12 +37,17 @@ class SignUpActivity :
             moveToNickname()
         }
         binding.toolbar.ivBack.setOnClickListener {
-            // TODO 다시 로그인 페이지로 이동
+            moveToLogIn()
         }
     }
 
     private fun moveToNickname() {
         startActivity(Intent(this, SignUpNicknameActivity::class.java))
+        finish()
+    }
+
+    private fun moveToLogIn(){
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
