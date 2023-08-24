a24b926db2f3bec02bc5fe86dd81b16fc3165084
jooyyoo <aliceyun9@naver.com>
2023-08-24 11:13:32 +0900

    [feat] #75 로그인 버튼 활성화 조건 구현

--- app/src/main/java/com/sopt/geonppang/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/login/LoginActivity.kt
@@ -2,30 +2,36 @@ package com.sopt.geonppang.presentation.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.ActivityLoginBinding
 import com.sopt.geonppang.presentation.signup.SignUpActivity
 import com.sopt.geonppang.util.binding.BindingActivity
 import com.sopt.geonppang.util.extension.hideKeyboard
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity :
     BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val viewModel: LogInViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         addListener()
     }
 
     private fun addListener() {
-        binding.root.setOnClickListener{
+        binding.root.setOnClickListener {
             hideKeyboard(it)
         }
-        binding.tvGotoSignup.setOnClickListener{
+        binding.tvGotoSignup.setOnClickListener {
             moveToSignup()
         }
     }
 
-    private fun moveToSignup(){
+    private fun moveToSignup() {
         startActivity(Intent(this, SignUpActivity::class.java))
         finish()
     }
