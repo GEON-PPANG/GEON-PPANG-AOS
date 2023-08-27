0c22fe6f66e8b8bd15b5f57e4b07e9571894df56
jihyunniiii <jita1125@naver.com>
2023-08-27 19:48:10 +0900

    [chore] #99 FilterInfoType 수정

--- app/src/main/java/com/sopt/geonppang/presentation/type/FilterInfoType.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/type/FilterInfoType.kt
@@ -2,18 +2,14 @@ package com.sopt.geonppang.presentation.type
 
 enum class FilterInfoType(
     val activityName: String,
-    val maxPage: Int,
 ) {
-    ONBOARDING(
-        "onBoarding",
-        6,
-    ),
     HOME(
         "home",
-        3,
     ),
     MYPAGE(
         "myPage",
-        3,
+    ),
+    BAKERYLIST(
+        "bakeryList"
     )
 }
