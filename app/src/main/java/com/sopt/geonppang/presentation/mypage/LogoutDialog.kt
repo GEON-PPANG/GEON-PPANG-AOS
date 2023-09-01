cd848ae966d099be109b8024259c00af602cf736
Dani43 <eldms0406@naver.com>
2023-09-01 13:38:59 +0900

    [feat] #104 중복 코드 BindingDialogFragment로 빼기

--- app/src/main/java/com/sopt/geonppang/presentation/mypage/LogoutDialog.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/mypage/LogoutDialog.kt
@@ -6,12 +6,11 @@ import android.view.View
 import android.view.WindowManager
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.DialogMiddleBinding
-import com.sopt.geonppang.presentation.SignActivity
+import com.sopt.geonppang.presentation.auth.SignActivity
 import com.sopt.geonppang.presentation.type.DialogType
 import com.sopt.geonppang.util.binding.BindingDialogFragment
 
-class LogoutDialog() : BindingDialogFragment<DialogMiddleBinding>(R.layout.dialog_middle) {
-
+class LogoutDialog : BindingDialogFragment<DialogMiddleBinding>(R.layout.dialog_middle) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -21,21 +20,14 @@ class LogoutDialog() : BindingDialogFragment<DialogMiddleBinding>(R.layout.dialo
 
     private fun initLayout() {
         binding.dialogType = DialogType.LOGOUT
-        dialog?.window?.setLayout(
-            WindowManager.LayoutParams.MATCH_PARENT,
-            WindowManager.LayoutParams.MATCH_PARENT,
-        )
-        dialog?.window?.setBackgroundDrawableResource(R.color.dialog_background)
-        dialog?.setCancelable(false)
-        dialog?.setCanceledOnTouchOutside(true)
     }
 
     private fun addListeners() {
-        binding.btnDialogLeft.setOnClickListener {
+        binding.btnDialogNo.setOnClickListener {
             dismiss()
         }
 
-        binding.btnDialogRight.setOnClickListener {
+        binding.btnDialogYes.setOnClickListener {
             dismiss()
             moveToSign()
         }
