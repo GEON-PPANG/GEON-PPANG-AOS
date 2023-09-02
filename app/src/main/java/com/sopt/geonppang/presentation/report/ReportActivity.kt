c2f6426be10fefc8e9cfabafda67b0be79d41855
jihyunniiii <jita1125@naver.com>
2023-09-02 18:05:33 +0900

    [feat] #109 editText 클릭 시 스크롤이 위로 올라오도록 구현

--- app/src/main/java/com/sopt/geonppang/presentation/report/ReportActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/report/ReportActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.ActivityReportBinding
 import com.sopt.geonppang.util.binding.BindingActivity
+import com.sopt.geonppang.util.extension.hideKeyboard
 
 class ReportActivity : BindingActivity<ActivityReportBinding>(R.layout.activity_report) {
     private val viewModel: ReportViewModel by viewModels()
@@ -18,6 +19,16 @@ class ReportActivity : BindingActivity<ActivityReportBinding>(R.layout.activity_
     }
 
     private fun addListeners() {
+        binding.etReportContent.setOnClickListener {
+            binding.scrollViewReport.smoothScrollTo(0, binding.etReportContent.top)
+        }
+
+        binding.etReportContent.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.scrollViewReport.smoothScrollTo(0, binding.etReportContent.top)
+            }
+        }
+
         binding.btnReport.setOnClickListener {
             showReportSucessBottomDialog()
         }
