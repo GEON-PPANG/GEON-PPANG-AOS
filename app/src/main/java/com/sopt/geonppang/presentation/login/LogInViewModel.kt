f8160b3c225872dda065ec66f09d25085efe68f5
jooyyoo <aliceyun9@naver.com>
2023-09-03 00:07:40 +0900

    [chore] #75 klint 적용

--- app/src/main/java/com/sopt/geonppang/presentation/login/LogInViewModel.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/login/LogInViewModel.kt
@@ -1,10 +1,7 @@
 package com.sopt.geonppang.presentation.login
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.map
-import com.sopt.geonppang.presentation.signup.SignUpViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
