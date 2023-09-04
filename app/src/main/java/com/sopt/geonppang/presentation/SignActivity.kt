fc9f177e578d8c72937ccae994c678691db52071
jooyyoo <aliceyun9@naver.com>
2023-09-04 21:40:00 +0900

    [feat] #115 카카오 로그인 붙이기 구현

--- app/src/main/java/com/sopt/geonppang/presentation/SignActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/SignActivity.kt
@@ -2,22 +2,31 @@ package com.sopt.geonppang.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
 import com.sopt.geonppang.R
+import com.sopt.geonppang.data.service.KakaoAuthService
 import com.sopt.geonppang.databinding.ActivitySignBinding
 import com.sopt.geonppang.presentation.login.LoginActivity
 import com.sopt.geonppang.presentation.signup.SignUpActivity
 import com.sopt.geonppang.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SignActivity :
     BindingActivity<ActivitySignBinding>(R.layout.activity_sign) {
+    @Inject
+    lateinit var kakaoAuthService : KakaoAuthService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        KakaoSdk.init(this, this.getString(R.string.kakao_app_key))
         addListeners()
     }
 
     private fun addListeners() {
         binding.btnStartWithKakao.setOnClickListener {
-            moveToSignUp()
+            kakaoAuthService.loginKakao()
         }
         binding.tvLoginWithEmail.setOnClickListener {
             moveToLogin()
