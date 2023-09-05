ac51797cb2e6991c5f97e734a89bf2b86a302bf9
Dani43 <eldms0406@naver.com>
2023-09-05 22:21:16 +0900

    [feat] #122 liveData -> flow로 migration 및 combine 이용해서 작성하기 버튼 활성화 조건에 대한 stateflow 생성 및 적용

--- app/src/main/java/com/sopt/geonppang/presentation/reviewWriting/ReviewWritingViewModel.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/reviewWriting/ReviewWritingViewModel.kt
@@ -1,8 +1,5 @@
 package com.sopt.geonppang.presentation.reviewWriting
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.geonppang.data.model.request.RequestReviewWriting
@@ -12,6 +9,13 @@ import com.sopt.geonppang.presentation.type.KeyWordType
 import com.sopt.geonppang.presentation.type.LikeType
 import com.sopt.geonppang.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.collections.set
@@ -20,19 +24,19 @@ import kotlin.collections.set
 class ReviewWritingViewModel @Inject constructor(private val reviewWritingRepository: ReviewWritingRepository) :
     ViewModel() {
 
-    val reviewText = MutableLiveData("")
-    private val _bakeryId: MutableLiveData<Int> = MutableLiveData()
-    val bakeryId: LiveData<Int> = _bakeryId
-    private val _isLike: MutableLiveData<LikeType> = MutableLiveData()
-    val isLike: LiveData<LikeType> = _isLike
-    private val _reviewSuccessState: MutableLiveData<UiState<Boolean>> = MutableLiveData()
-    val reviewSuccessState: LiveData<UiState<Boolean>> = _reviewSuccessState
-    private val _reviewCancelState: MutableLiveData<Boolean> = MutableLiveData()
-    val reviewCancelState: LiveData<Boolean> = _reviewCancelState
-    private val _bakeryInfo: MutableLiveData<BakeryReviewWritingInfo> = MutableLiveData()
-    val bakeryInfo: LiveData<BakeryReviewWritingInfo> = _bakeryInfo
-    val userKeyWordType: MutableLiveData<Map<KeyWordType, Boolean>> =
-        MutableLiveData(
+    val reviewText = MutableStateFlow("")
+    private val _bakeryId = MutableStateFlow(-1)
+    val bakeryId get() = _bakeryId.asStateFlow()
+    private val _isLike = MutableStateFlow<LikeType?>(null)
+    val isLike get() = _isLike.asStateFlow()
+    private val _reviewSuccessState = MutableStateFlow<UiState<Boolean>>(UiState.Loading)
+    val reviewSuccessState get() = _reviewSuccessState
+    private val _reviewCancelState = MutableStateFlow<Boolean?>(null)
+    val reviewCancelState get() = _reviewCancelState
+    private val _bakeryInfo = MutableStateFlow<BakeryReviewWritingInfo?>(null)
+    val bakeryInfo get() = _bakeryInfo
+    val userKeyWordType: MutableStateFlow<Map<KeyWordType, Boolean>> =
+        MutableStateFlow(
             mapOf(
                 KeyWordType.DELICIOUS to false,
                 KeyWordType.KIND to false,
@@ -40,27 +44,34 @@ class ReviewWritingViewModel @Inject constructor(private val reviewWritingReposi
                 KeyWordType.ZERO_WASTE to false
             )
         )
-    val isUserKeyWordTypeSelected: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
-        addSource(userKeyWordType) { keyWordMap ->
-            value = keyWordMap.any { it.value }
-        }
-    }
+    val isUserKeyWordTypeSelected: StateFlow<Boolean> = userKeyWordType.map { keywordMap ->
+        keywordMap.values.any { it }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), false)
+    val isReviewWritingEnabled: StateFlow<Boolean> = combine(
+        isLike,
+        isUserKeyWordTypeSelected,
+        reviewText
+    ) { isLike, isUserKeyWordTypeSelected, reviewText ->
+        val isLikeEnabled = isLike == LikeType.LIKE && isUserKeyWordTypeSelected
+        val isBadEnabled = isLike == LikeType.BAD && reviewText.isNotBlank()
+        isLikeEnabled || isBadEnabled
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), false)
 
     fun setBakeryId(bakeryId: Int) {
         _bakeryId.value = bakeryId
-    }
-
-    fun setReviewCancelState(isCancel: Boolean) {
-        _reviewCancelState.value = isCancel
     }
 
     fun setLikeType(isLike: LikeType) {
         _isLike.value = isLike
     }
 
+    fun setReviewCancelState(isCancel: Boolean) {
+        _reviewCancelState.value = isCancel
+    }
+
     fun setKeyWordType(keyWordType: KeyWordType) {
-        val isSelected = userKeyWordType.value?.get(keyWordType) ?: return
-        userKeyWordType.value = userKeyWordType.value?.toMutableMap()?.apply {
+        val isSelected = userKeyWordType.value[keyWordType] ?: return
+        userKeyWordType.value = userKeyWordType.value.toMutableMap().apply {
             this[keyWordType] = !isSelected
         }
     }
