91ca0500bdab6d77b3eb49ee92b6537fd5814d52
jooyyoo <aliceyun9@naver.com>
2023-09-14 01:52:21 +0900

    [feat] #123 회원가입 response 수정

--- app/src/main/java/com/sopt/geonppang/data/model/response/ResponseSignup.kt
+++ app/src/main/java/com/sopt/geonppang/data/model/response/ResponseSignup.kt
@@ -1,14 +1,25 @@
 package com.sopt.geonppang.data.model.response
 
+import com.sopt.geonppang.domain.model.SignInfo
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ResponseSignup(
     val code: String,
     val message: String,
     val data: ResponseSignupData,
 ) {
+    @Serializable
     data class ResponseSignupData(
         val memberId: Int,
         val type: String,
         val email: String,
         val role: String,
+    )
+
+    fun toSignup() = SignInfo(
+        role = data.role,
+        type = data.type,
+        email = data.email,
     )
 }
---
+++ app/src/main/java/com/sopt/geonppang/domain/model/SignInfo.kt
@@ -0,0 +1,10 @@
+package com.sopt.geonppang.domain.model
+
+
+data class SignInfo(
+    val role: String,
+    val type: String,
+    val email: String,
+    val accessToken: String = "",
+    val refreshToken: String = "",
+)
