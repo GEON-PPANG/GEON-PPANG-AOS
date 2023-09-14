53dfaa0fbf48bb22679d93c612b6dc480a1bb860
jooyyoo <aliceyun9@naver.com>
2023-09-14 14:40:50 +0900

    [feat] #141 로그인 서버 통신 POST 서비스 추가

--- app/src/main/java/com/sopt/geonppang/data/service/AuthService.kt
+++ app/src/main/java/com/sopt/geonppang/data/service/AuthService.kt
@@ -4,6 +4,7 @@ import com.sopt.geonppang.data.model.request.RequestSignup
 import com.sopt.geonppang.data.model.response.ResponseLogout
 import com.sopt.geonppang.data.model.response.ResponseSignup
 import com.sopt.geonppang.data.model.response.ResponseWithdraw
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.Header
@@ -21,4 +22,7 @@ interface AuthService {
 
     @POST("auth/logout")
     suspend fun logout(): ResponseLogout
+
+    @POST("auth/login")
+    suspend fun login() : Response<Unit>
 }
