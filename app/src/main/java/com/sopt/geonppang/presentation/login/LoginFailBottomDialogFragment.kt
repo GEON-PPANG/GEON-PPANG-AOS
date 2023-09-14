e2ee84bca07772ff6464ce14d41e0eb073f3fd7f
jooyyoo <aliceyun9@naver.com>
2023-09-15 02:06:20 +0900

    [feat] #141 로그인 실패 바텀 시트 프래그먼트 구현

---
+++ app/src/main/java/com/sopt/geonppang/presentation/login/LoginFailBottomDialogFragment.kt
@@ -0,0 +1,24 @@
+package com.sopt.geonppang.presentation.login
+
+import android.os.Bundle
+import android.view.View
+import com.sopt.geonppang.R
+import com.sopt.geonppang.databinding.DialogBottomLoginFailBinding
+import com.sopt.geonppang.util.binding.BindingBottomSheetDialogFragment
+
+class LoginFailBottomDialogFragment : BindingBottomSheetDialogFragment<DialogBottomLoginFailBinding>(
+    R.layout.dialog_bottom_login_fail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.btnLoginFailConfirm.setOnClickListener {
+            dismiss()
+            requireActivity().finish()
+        }
+    }
+}
