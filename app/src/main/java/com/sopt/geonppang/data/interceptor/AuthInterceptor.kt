7703de317584cabfe43705cea2284e7f02a52c79
Dani43 <eldms0406@naver.com>
2023-09-17 01:02:46 +0900

    [feat] #153 실제 refresh token AuthInterceptor의 header로 추가

--- app/src/main/java/com/sopt/geonppang/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/sopt/geonppang/data/interceptor/AuthInterceptor.kt
@@ -1,16 +1,18 @@
 package com.sopt.geonppang.data.interceptor
 
-import com.sopt.geonppang.BuildConfig
+import com.sopt.geonppang.data.datasource.local.GPDataStore
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
-class AuthInterceptor @Inject constructor() : Interceptor {
+class AuthInterceptor @Inject constructor(
+    private val gpDataStore: GPDataStore
+) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val authRequest =
-            originalRequest.newBuilder().addHeader("Authorization", BuildConfig.ACCESS_TOKEN)
+            originalRequest.newBuilder().addHeader("Authorization", gpDataStore.accessToken)
                 .build()
         val response = chain.proceed(authRequest)
 
