f972aadd526dad3e20dc14e22a7a078ae4ed861e
jooyyoo <aliceyun9@naver.com>
2023-09-19 03:11:15 +0900

    [chore] #147 klint 반영

--- app/src/main/java/com/sopt/geonppang/presentation/auth/SignUpActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/auth/SignUpActivity.kt
@@ -3,7 +3,6 @@ package com.sopt.geonppang.presentation.auth
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
 import com.sopt.geonppang.R
 import com.sopt.geonppang.databinding.ActivitySignupBinding
 import com.sopt.geonppang.util.binding.BindingActivity
@@ -40,7 +39,6 @@ class SignUpActivity :
             finish()
         }
         binding.tvEmailErrorMsg.setOnClickListener {
-
         }
     }
 
