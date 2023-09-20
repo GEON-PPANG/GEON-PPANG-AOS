6035c8c08431c4fa2203338e8ce2d3ad349d16d0
Dani43 <eldms0406@naver.com>
2023-09-20 17:50:43 +0900

    [mod] #172 자체 회원가입 후 로그인 페이지로 이동하도록 수정

--- app/src/main/java/com/sopt/geonppang/presentation/filterSetting/MainPurposeFilterFragment.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/filterSetting/MainPurposeFilterFragment.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import com.sopt.geonppang.R
+import com.sopt.geonppang.data.datasource.local.GPDataSource
 import com.sopt.geonppang.databinding.FragmentMainPurposeFilterBinding
 import com.sopt.geonppang.presentation.MainActivity
+import com.sopt.geonppang.presentation.auth.SignActivity
+import com.sopt.geonppang.presentation.type.PlatformType
 import com.sopt.geonppang.util.binding.BindingFragment
 
 class MainPurposeFilterFragment :
@@ -24,12 +27,28 @@ class MainPurposeFilterFragment :
 
     private fun addListeners() {
         binding.layoutMainPurposeSkip.setOnClickListener {
-            moveToMain()
+            val gpDataSource = GPDataSource(requireContext())
+            when (gpDataSource.platformType) {
+                PlatformType.NONE.name -> {
+                    moveToSign()
+                }
+
+                PlatformType.KAKAO.name -> {
+                    moveToMain()
+                }
+            }
         }
     }
 
     private fun moveToMain() {
         val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.flags =
+            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+    private fun moveToSign() {
+        val intent = Intent(requireContext(), SignActivity::class.java)
         intent.flags =
             Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
