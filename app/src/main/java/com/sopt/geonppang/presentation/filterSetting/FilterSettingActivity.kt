e3f5805fdecc1f8cb5c7261a88d7a9c49f24c6ab
Dani43 <eldms0406@naver.com>
2023-09-21 19:22:10 +0900

    [chore] #181 ktlint 적용

--- app/src/main/java/com/sopt/geonppang/presentation/filterSetting/FilterSettingActivity.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/filterSetting/FilterSettingActivity.kt
@@ -42,12 +42,12 @@ class FilterSettingActivity : BindingActivity<ActivityFilterBinding>(R.layout.ac
         binding.vpFilterContainer.adapter = adapter
         binding.vpFilterContainer.isUserInputEnabled = false
         binding.vpFilterContainer.registerOnPageChangeCallback(object :
-            ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                viewModel.setCurrentPage(position)
-            }
-        })
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    viewModel.setCurrentPage(position)
+                }
+            })
 
         setPreviousActivity()
     }
