7fdd436212c4e671871d1cb7c0917730d7154ad8
jooyyoo <aliceyun9@naver.com>
2023-09-26 21:40:48 +0900

    [feat] #199 필터 설정 엠플리튜드 이벤트 트래킹을 위한 파일 생성 리소스 추가

--- app/src/main/java/com/sopt/geonppang/presentation/type/NutrientFilterType.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/type/NutrientFilterType.kt
@@ -4,15 +4,19 @@ import androidx.annotation.StringRes
 import com.sopt.geonppang.R
 
 enum class NutrientFilterType(
+    @StringRes val amplitudeRes: Int,
     @StringRes val titleRes: Int
 ) {
     NUTRIENT(
+        R.string.nutrient_type_nutrient_open_ampl,
         R.string.nutrient_type_nutrient_open
     ),
     INGREDIENT(
+        R.string.nutrient_type_ingredient_open_ampl,
         R.string.nutrient_type_ingredient_open
     ),
     NOT(
+        R.string.nutrient_type_not_open_ampl,
         R.string.nutrient_type_not_open
     )
 }
