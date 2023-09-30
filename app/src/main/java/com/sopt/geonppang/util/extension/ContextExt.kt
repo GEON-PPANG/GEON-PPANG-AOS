103ec2de8b4b6374338e186cd3ff1357ac4abf95
Dani43 <eldms0406@naver.com>
2023-09-30 12:56:11 +0900

    [chore] #212 ktlint 적용

--- app/src/main/java/com/sopt/geonppang/util/extension/ContextExt.kt
+++ app/src/main/java/com/sopt/geonppang/util/extension/ContextExt.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
-import android.widget.Toast
 import coil.Coil
 import coil.request.ImageRequest
 
