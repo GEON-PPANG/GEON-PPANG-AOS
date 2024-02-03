ed1d8c3d2775bd6843e654c9e3f76f68b2c2f45a
jihyunniiii <jita1125@naver.com>
2024-02-03 20:41:37 +0900

    [chore] #218 NutrientFilterType에 id 추가

--- app/src/main/java/com/sopt/geonppang/presentation/type/NutrientFilterType.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/type/NutrientFilterType.kt
@@ -4,18 +4,22 @@ import androidx.annotation.StringRes
 import com.sopt.geonppang.R
 
 enum class NutrientFilterType(
+    val id: Int,
     @StringRes val titleRes: Int,
     @StringRes val desRes: Int
 ) {
     NUTRIENT(
+        id = 2,
         titleRes = R.string.nutrient_type_nutrient_open_title,
         desRes = R.string.nutrient_type_nutrient_open_des
     ),
     INGREDIENT(
+        id = 1,
         titleRes = R.string.nutrient_type_ingredient_open_title,
         desRes = R.string.nutrient_type_ingredient_open_des
     ),
     ALL(
+        id = 3,
         titleRes = R.string.nutrient_type_all_open_title,
         desRes = R.string.nutrient_type_all_open_des
     )
