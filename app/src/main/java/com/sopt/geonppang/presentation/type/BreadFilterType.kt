e95064a14dab3eac23801963a71fe77d8e0541e0
jihyunniiii <jita1125@naver.com>
2024-02-03 17:29:09 +0900

    [chore] #218 BreadFilterType에 id 추가

--- app/src/main/java/com/sopt/geonppang/presentation/type/BreadFilterType.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/type/BreadFilterType.kt
@@ -4,22 +4,27 @@ import androidx.annotation.StringRes
 import com.sopt.geonppang.R
 
 enum class BreadFilterType(
+    val id: Int,
     @StringRes val titleRes: Int,
     @StringRes val desRes: Int,
 ) {
     GLUTENFREE(
+        id = 1,
         titleRes = R.string.bread_type_gluten_free_title,
         desRes = R.string.bread_type_gluten_free_des
     ),
     VEGAN(
+        id = 2,
         titleRes = R.string.bread_type_vegan_title,
         desRes = R.string.bread_type_vegan_des
     ),
     NUTFREE(
+        id = 3,
         titleRes = R.string.bread_type_nut_free_title,
         desRes = R.string.bread_type_nut_free_des
     ),
     SUGARFREE(
+        id = 4,
         titleRes = R.string.bread_type_sugar_free_title,
         desRes = R.string.bread_type_sugar_free_des,
     )
