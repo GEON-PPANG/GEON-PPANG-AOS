c8bcfa874d2752167045284ed50da78ea3a1c3e9
jihyunniiii <jita1125@naver.com>
2024-02-04 22:19:26 +0900

    [chore] #218 NutrientFilterType 네이밍 변경

--- app/src/main/java/com/sopt/geonppang/presentation/type/NutrientFilterType.kt
+++ app/src/main/java/com/sopt/geonppang/presentation/type/NutrientFilterType.kt
@@ -8,17 +8,17 @@ enum class NutrientFilterType(
     @StringRes val titleRes: Int,
     @StringRes val desRes: Int
 ) {
-    NUTRIENT(
+    MATERIALS(
         id = 2,
         titleRes = R.string.nutrient_type_nutrient_open_title,
         desRes = R.string.nutrient_type_nutrient_open_des
     ),
-    INGREDIENT(
+    INGREDIENTS(
         id = 1,
         titleRes = R.string.nutrient_type_ingredient_open_title,
         desRes = R.string.nutrient_type_ingredient_open_des
     ),
-    ALL(
+    PRIVATE(
         id = 3,
         titleRes = R.string.nutrient_type_all_open_title,
         desRes = R.string.nutrient_type_all_open_des
