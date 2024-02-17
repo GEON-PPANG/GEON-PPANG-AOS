ecffe6e5cf09783b2451c6525948df6b618d1301
Dan2dani <eldms0406@naver.com>
2024-02-17 14:18:34 +0900

    [feat] #224 breadType에 대해 List 형태로 가지고 있는 엔티티 생성 List로 전부 전환 후 기존 엔티티 삭제 예정

--- app/src/main/java/com/sopt/geonppang/domain/model/Bakery.kt
+++ app/src/main/java/com/sopt/geonppang/domain/model/Bakery.kt
@@ -1,5 +1,8 @@
 package com.sopt.geonppang.domain.model
 
+import com.sopt.geonppang.presentation.type.BreadFilterType
+
+// TODO: breadType 형태 완전 변환 후 차후 삭제
 data class Bakery(
     val bakeryId: Int,
     val bakeryName: String,
@@ -11,4 +14,18 @@ data class Bakery(
     val bookmarkCount: Int?,
     val bakeryPicture: String,
     val breadType: BreadType,
+)
+
+// TODO: 차후 네이밍 수정
+data class BakeryInformation(
+    val bakeryId: Int,
+    val bakeryName: String,
+    val isHACCP: Boolean,
+    val isVegan: Boolean,
+    val isNonGMO: Boolean,
+    val firstNearStation: String,
+    val secondNearStation: String,
+    val bookmarkCount: Int?,
+    val bakeryPicture: String,
+    val breadTypeList: List<BreadFilterType>,
 )
